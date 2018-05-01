ad29d5d28de492825d958bca350ea557c28214da
henAutumn <33758290+henAutumn@users.noreply.github.com>
2018-05-01 15:21:27 -0400

    ah is fixing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
+import {ProductsComponent} from './products/products.component';
+import {AboutComponent} from './about/about.component';
+import{AppComponent} from './app.component'
+
+const routes:Routes=[
+  {path:'products', component:ProductsComponent},
+  {path:'about', component:AboutComponent},
+  {path:'home', component:AppComponent}
+]
+
 @NgModule({
+  imports:[RouterModule.forRoot(routes)],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
