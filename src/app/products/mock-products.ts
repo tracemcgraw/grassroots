d055569016f1d5d7fd9a819b48959b7ae2f6252b
unknown <mcgraw.trace@gmail.com>
2018-05-01 14:25:06 -0400

    table complete

--- src/app/products/mock-products.ts
+++ src/app/products/mock-products.ts
@@ -1,13 +1,13 @@
 import { Product } from "./products";
 
 export const PRODUCTS: Product[] = [
-    { name: "Switch", price: 400.00, isInStock: true},
-    {name: "DS", price: 400.00, isInStock: true},
-    {name: "DS 3D", price: 400.00, isInStock: true},
-    {name: "GameCube", price: 400.00, isInStock: true},
-    {name: "Wii", price: 400.00, isInStock: true},
-    {name: "Will U", price: 400.00, isInStock: true},
-    { name: "Game Boy Color", price: 400.00, isInStock: true},
+    { name: "Pots", price: 20.00, isInStock: true},
+    {name: "Soil", price: 10.00, isInStock: true},
+    {name: "Seed Packs", price: 10.00, isInStock: true},
+    {name: "Rake", price: 15.00, isInStock: true},
+    {name: "Rocks", price: 10.00, isInStock: true},
+    {name: "Wood", price: 15.00, isInStock: true},
+    { name: "Hoe", price: 400.00, isInStock: true},
 
 
 ]
