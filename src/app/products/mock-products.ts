e65ba9a174c27fd3fef073ac5fd6aa0d48f90bd7
unknown <mcgraw.trace@gmail.com>
2018-05-01 14:05:53 -0400

    adding table products

---
+++ src/app/products/mock-products.ts
@@ -0,0 +1,13 @@
+import { Product } from "./products";
+
+export const PRODUCTS: Product[] = [
+    { name: "Switch", price: 400.00, isInStock: true},
+    {name: "DS", price: 400.00, isInStock: true},
+    {name: "DS 3D", price: 400.00, isInStock: true},
+    {name: "GameCube", price: 400.00, isInStock: true},
+    {name: "Wii", price: 400.00, isInStock: true},
+    {name: "Will U", price: 400.00, isInStock: true},
+    { name: "Game Boy Color", price: 400.00, isInStock: true},
+
+
+]
